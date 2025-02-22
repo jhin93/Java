889a8ebfdb2c974d2234285e320227e0f240d4a9
jhin93 <scene1993@gmail.com>
2025-02-22 22:11:23 +1100

    equals method is for test

--- module2/People/src/Person.java
+++ module2/People/src/Person.java
@@ -1,4 +1,32 @@
-
+/*
+ * Represents a person, with a name and age
+ */
 public class Person {
+	//instance variables
+    /*
+     * The name of the person
+     */
+	String name;
+    /*
+     * The age of the person
+     */
+	int age;
+	
+	
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
+    /*
+     * Compare instances of Person based on name
+     */
+    
+    public boolean equals(Object something) { // equals method is used to assertEquals()
+        // cast given object to Person
+        Person otherPerson = (Person) something;
+        
+        //Returns true if names of both persons are the same
+        return this.name.equals(otherPerson.name);
+    }
 }
---
+++ module2/People/src/PersonTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonTest {
+
+	@Test
+	void testPerson() {
+		Person person1 = new Person("Ted", 22);
+		Person person2 = new Person("ted", 22);
+
+		//assertEquals uses === to compare primitives
+		// let's compare the ages of person1 and person2
+		assertEquals(person1.age, person2.age);
+
+		// assertEquals uses equals() method implemented in Person class
+		assertNotEquals(person1, person2); // Not same name because of capital T
+		
+		Person person3 = new Person("Ted", 34);
+
+		assertEquals(person1, person3); // Same name with capital T
+	}
+}
